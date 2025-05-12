fba8514e174bd81fc342aeeba22fe909b92ead01
BENELMALIH Ahmed Amine <amine.ahmed.benel@gmail.com>
2025-05-12 10:46:09 +0100

    Programme_base

--- public/js/app.js
+++ public/js/app.js
@@ -15,47 +15,59 @@
 // ## 3 - Instructions:
 // - Account Creation and Management:
 //     + Allow the user, via prompts, to choose between signing up, logging in, or changing the password.
-//     + If the user only writes "exit," they exit the current process, and the choice question is asked again.
-//         * If the user chooses to sign up, here are the details they must enter:
-//             # Name (Full):
-//             - Check for leading or trailing spaces.
-//             - The first letter should be capitalized.
-//             - After each space, the first letter should remain capitalized.
-//             - Check that all other characters are in lowercase.
-//             - Do not save the Name if it has less than 5 characters (excluding spaces).
-//             - Do not save the Name if it contains numbers, "@", or similar special characters.
+let enter = prompt("- Signing 's'\n- Logging 'l'\n- Changing the password 'p'\n- Exit 'e'")
 
-//             # Email:
-//             - Check for leading or trailing spaces.
-//             - Convert all letters to lowercase.
-//             - Do not save the Email if it has spaces in the middle.
-//             - Do not save the Email if it has fewer than 10 characters (excluding spaces).
-//             - Do not save the Email if it does not contain exactly one "@" symbol.
-//             - Ensure the email is unique.
+if (enter === 'e' || enter === null || enter === '') {
+    //     + If the user only writes "exit," they exit the current process, and the choice question is asked again.
+    alert("See you later..")
+} else if (enter === 's') {
+    //         * If the user chooses to sign up, here are the details they must enter:
+    //             # Name (Full):
+    //             - Check for leading or trailing spaces.
+    //             - The first letter should be capitalized.
+    //             - After each space, the first letter should remain capitalized.
+    //             - Check that all other characters are in lowercase.
+    //             - Do not save the Name if it has less than 5 characters (excluding spaces).
+    //             - Do not save the Name if it contains numbers, "@", or similar special characters.
+    
+    //             # Email:
+    //             - Check for leading or trailing spaces.
+    //             - Convert all letters to lowercase.
+    //             - Do not save the Email if it has spaces in the middle.
+    //             - Do not save the Email if it has fewer than 10 characters (excluding spaces).
+    //             - Do not save the Email if it does not contain exactly one "@" symbol.
+    //             - Ensure the email is unique.
+    
+    //             # Age:
+    //             - Check for leading, trailing, or middle spaces.
+    //             - Verify that only digits are entered.
+    //             - Do not save the Age if it has 0 characters, or if it has 3 characters or more.
+    
+    //             # Password:
+    //             - Check for leading or trailing spaces.
+    //             - Do not save the Password if it has spaces in the middle.
+    //             - Require at least one special character from the set: ["@", "#", "-", "+", "*", "/"].
+    //             - Require at least 7 characters to confirm the password.
+    
+    //             # Password_confirmed:
+    //             - The user must re-enter their exact password; otherwise, they are blocked.
+    
+} else if (enter === 'l') {
+    //         * If the user chooses to log in, here are the details they must enter:
+    //             # Email:
+    //             - Check if the email exists in our Database.
+                
+    //             # Password:
+    //             - Check if the entered password is associated with the previously entered email.
 
-//             # Age:
-//             - Check for leading, trailing, or middle spaces.
-//             - Verify that only digits are entered.
-//             - Do not save the Age if it has 0 characters, or if it has 3 characters or more.
+} else if (enter === 'p') {
+    //         * If the user chooses to change the password:
+    //             - They must enter their existing Email in the Database.
 
-//             # Password:
-//             - Check for leading or trailing spaces.
-//             - Do not save the Password if it has spaces in the middle.
-//             - Require at least one special character from the set: ["@", "#", "-", "+", "*", "/"].
-//             - Require at least 7 characters to confirm the password.
-
-//             # Password_confirmed:
-//             - The user must re-enter their exact password; otherwise, they are blocked.
-
-//         * If the user chooses to log in, here are the details they must enter:
-//             # Email:
-//             - Check if the email exists in our Database.
-            
-//             # Password:
-//             - Check if the entered password is associated with the previously entered email.
-
-//         * If the user chooses to change the password:
-//             - They must enter their existing Email in the Database.
+} else {
+    alert("Please choose from options")
+    enter = prompt("- Signing ('s')\n- Logging ('l')\n- Changing the password ('p')\n- Exit ('e')")
+}
 
 //         * After the user logs in, display the amount they have in their bank (user's choice) and offer them services:
 //             # Logout:
